722a41889d357021985e7ff65b09304ea02a58f8
Ashutosh Panda <ashu.juga99@gmail.com>
2019-02-20 20:49:49 +0530

    Functionality Done with JavaScript

--- js/main.js
+++ js/main.js
@@ -11,10 +11,71 @@ const scale = 1.5,
 
 //RTender Page
 
-const renderpage = num => {};
+const renderpage = num => {
+  pageIsRendering = true;
 
+  //Get Page
+  pdfDoc.getPage(num).then(page => {
+    //console.log(page);
+    //set scale
+    const viewport = page.getViewport({ scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderCtx = {
+      canvasContext: ctx,
+      viewport
+    };
+    page.render(renderCtx).promise.then(() => {
+      pageIsRendering = false;
+
+      if (pageNumIsPending !== null) {
+        renderpage(pageNumIsPending);
+        pageNumIsPending = null;
+      }
+    });
+
+    //Output Current Page
+    document.querySelector('#page-num').textContent = num;
+  });
+};
+
+//Check for Page rendering
+const queueRenderPage = num => {
+  if (pageIsRendering) {
+    pageNumIsPending = num;
+  } else {
+    renderpage(num);
+  }
+};
+//show prev page
+const showPrevPage = () => {
+  if (pageNum <= 1) {
+    return;
+  }
+  pageNum--;
+  queueRenderPage(pageNum);
+};
+
+//show next page
+const showNextPage = () => {
+  if (pageNum >= pdfDoc.numPages) {
+    return;
+  }
+  pageNum++;
+  queueRenderPage(pageNum);
+};
 //Get Document
+
 pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
   pdfDoc = pdfDoc_;
-  console.log(pdfDoc);
+  //console.log(pdfDoc);
+
+  document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+  renderpage(pageNum);
 });
+
+//Button Events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage);
+document.querySelector('#next-page').addEventListener('click', showNextPage);
